e101c6030726ab324a7ab74757df167ce5f06568
Maria Mozgunova <bagira_2003@inbox.ru>
2021-01-02 19:49:19 +0300

    main.yml: Add Slack notification

--- hello.py
+++ hello.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'I did it!'
+    return 'I did it! You can read more about this app -> ' + \
+           'https://github.com/MariaMozgunova/flask-app'
 
 
 if __name__ == '__main__':
